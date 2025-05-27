29df8ea9bafea16bf13eafc7927dbe8d5ee6afca
Resty <restypabello@gmail.com>
2025-05-27 11:39:01 +0800

    Add generated item, API routes, and migration models

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Request\FormRequest\ItemRequest;
+use App\Http\Resources\ItemResource;
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -12,7 +14,9 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::all();
+
+        return ItemResource::collection($items);
     }
 
     /**

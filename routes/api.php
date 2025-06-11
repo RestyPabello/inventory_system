52f743f4d9aef2413c29244ae99a54212ddb532b
Resty <restypabello@gmail.com>
2025-06-11 22:17:47 +0800

    Add API versioning with v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::middleware('auth:api')->group( function () {
-    Route::apiResource('items', ItemController::class);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:api')->group( function () {
+        Route::apiResource('items', ItemController::class);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
     });
-});
 
-Route::prefix('users')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::prefix('users')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
 });
